26a4f8a116ceb6eb07e465ba3db1f29788ee2376
LAPTOP-R0HO03P8\daaru <arihasan707@gmail.com>
2025-04-17 13:40:50 +0700

    update composer 1

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '63cec486b7b2b34dc0de502984cdc6069224ed17',
+        'reference' => 'ac2af99037ece5bd5a06adef8f02016d8cd37ed8',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -442,7 +442,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '63cec486b7b2b34dc0de502984cdc6069224ed17',
+            'reference' => 'ac2af99037ece5bd5a06adef8f02016d8cd37ed8',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
