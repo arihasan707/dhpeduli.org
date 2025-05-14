31361c6b3abcf62abfca6c0e9ffd4eaaccc105c3
LAPTOP-R0HO03P8\daaru <arihasan707@gmail.com>
2025-05-14 17:05:23 +0700

    add verifikasi meta pixel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="facebook-domain-verification" content="1nkdm2m5z62vn05fmxxb1sjvlyzbv5" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <meta name="description" content="Bersama tangan peduli lahirkan Generasi Qur'ani">
